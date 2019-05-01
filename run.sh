60c9ad79bb2e351eac2201df1331f335a22419b6
dmitry <mitia810@mail.ru>
2019-05-01 16:36:30 +0300

    Travis

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 sudo cmake CMakeLists.txt
 sudo make
 
-g++ tests.cpp -std=c++11 -o tests -lgtest -lpthread
-g++ main.cpp -std=c++11 -o main
+g++ tests.cpp -std=c++14 -o tests -lgtest -lpthread
+g++ main.cpp -std=c++14 -o main
 
 ./tests
 ./main
