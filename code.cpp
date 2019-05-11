902224687ad67b7b5059de571f0e269d754bb449
dmitry <mitia810@mail.ru>
2019-05-12 01:56:17 +0300

    test

--- code.cpp
+++ code.cpp
@@ -227,7 +227,7 @@ class Game{
             Army* new_army = new Army();
             new_army->add_soldiers(factory.create_infantry());
             new_army->add_tank(factory.create_tanks());
-            new_army->add_artillery (factory.create_artillery());
+            new_army->add_artillery(factory.create_artillery());
             return new_army;
         }
 };
